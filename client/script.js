868163fb82a5c4f5bb54ac5efaa9080cde78172a
jordanjc-ShirtHanger <devvcarter4ever24@outlook.com>
2024-10-16 13:59:24 -0500

    Temporary Axios call and population of search results code, until HTML and CSS are finalized

--- client/script.js
+++ client/script.js
@@ -1,14 +1,59 @@
+/* INSERT DOM VARIABLES HERE */
+
+
+/* INSERT EVENT LISTENERS HERE */
+
 searchButton.addEventListener('click', async () => { 
 
     let userInput = userInput.value // Collects search results for user input
     /* Consider adding .toLowerCase() */
 
-    let responseSearch = await axios.get(`http://localhost:3001/${userInput}`)
+    let response = await axios.get(`http://localhost:3001/products/name/${userInput}`)
 
-    let responseID = await axios.get(`http://localhost:3001/${userInput}`)
 
-    let itemData = responseSearch.data
+    let productsArray = response.data
 
-    let itemPic = itemData.imageUrl
+    searchResultsContainer.innerHTML = ``
+    /* Meant to clear previous search results */
+
+    for (product of productsArray) { /* This should populate the bottom of the page with ALL
+        search results Whenever search button is clicked. */
+
+        let responseBrand = await axios.get(`http://localhost:3001/brands/${product.brand_id}`)
+
+        let productName = product.name
+        let productPrice = product.price
+        let productDesc = product.description
+        let productPic = product.imageUrl
+        let productColor = product.color
+
+        productLogo = responseBrand.data.logo
+
+        /* Confirmation of data */
+        console.log('==================')
+        console.log(productName)
+        console.log(productPrice)
+        console.log(productDesc)
+        console.log(productPic)
+        console.log(productColor)
+        console.log(productLogo)
+        console.log('==================')
+
+        /* Temporary name and format until HTML is finalized */
+
+        let searchResultItem = document.createElement('article')
+        searchResultItem.innerHTML = 
+        `<img class="search-result-image" id="brand-image" src="${productLogo}" alt="TEST BRAND IMAGE">
+        <img class="search-result-image" id="product-image" src="${productPic}" alt="TEST ITEM IMAGE">
+        <h3>${productName} - (${productColor})</h3>
+        <h4>$${productPrice}</h4>
+        <p>${productDesc}</p>`
+
+        searchResultsContainer.appendChild(searchResultItem)
+    }
+
     /* Confirmation of data */
 })
+
+
+/* INSER FUNCTIONS HERE (Normal function notation, no arrows, so it can be hoisted) */
