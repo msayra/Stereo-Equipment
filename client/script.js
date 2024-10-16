915a045ce3a9b45cbe167245e49d203bb6e699f0
jordanjc-ShirtHanger <devvcarter4ever24@outlook.com>
2024-10-16 15:34:03 -0500

    Axio calls and data filling is WORKING

--- client/script.js
+++ client/script.js
@@ -1,11 +1,13 @@
 /* INSERT DOM VARIABLES HERE */
 
-
+const searchResultsContainer = document.querySelector('.search-results-container')
+const productSearchBar = document.querySelector('#product-search-bar')
+const productSearchButton = document.querySelector('#product-search-button')
 /* INSERT EVENT LISTENERS HERE */
 
-searchButton.addEventListener('click', async () => { 
+productSearchButton.addEventListener('click', async () => { 
 
-    let userInput = userInput.value // Collects search results for user input
+    let userInput = productSearchBar.value // Collects search results for user input
     /* Consider adding .toLowerCase() */
 
     let response = await axios.get(`http://localhost:3001/products/name/${userInput}`)
@@ -13,7 +15,8 @@ searchButton.addEventListener('click', async () => {
 
     let productsArray = response.data
 
-    searchResultsContainer.innerHTML = ``
+    ClearSearchResults(searchResultsContainer)
+
     /* Meant to clear previous search results */
 
     for (product of productsArray) { /* This should populate the bottom of the page with ALL
@@ -28,6 +31,7 @@ searchButton.addEventListener('click', async () => {
         let productColor = product.color
 
         productLogo = responseBrand.data.logo
+        brandName = responseBrand.data.name
 
         /* Confirmation of data */
         console.log('==================')
@@ -41,15 +45,7 @@ searchButton.addEventListener('click', async () => {
 
         /* Temporary name and format until HTML is finalized */
 
-        let searchResultItem = document.createElement('article')
-        searchResultItem.innerHTML = 
-        `<img class="search-result-image" id="brand-image" src="${productLogo}" alt="TEST BRAND IMAGE">
-        <img class="search-result-image" id="product-image" src="${productPic}" alt="TEST ITEM IMAGE">
-        <h3>${productName} - (${productColor})</h3>
-        <h4>$${productPrice}</h4>
-        <p>${productDesc}</p>`
-
-        searchResultsContainer.appendChild(searchResultItem)
+        appendSearchResults(searchResultsContainer, productLogo, brandName, productPic, productName, productColor, productPrice, productDesc)
     }
 
     /* Confirmation of data */
@@ -57,3 +53,24 @@ searchButton.addEventListener('click', async () => {
 
 
 /* INSER FUNCTIONS HERE (Normal function notation, no arrows, so it can be hoisted) */
+
+
+function ClearSearchResults(searchResultsContainer) {
+    searchResultsContainer.innerHTML = ``
+}
+
+function appendSearchResults(searchResultsContainer, productLogo, brandName, productPic, productName, productColor, productPrice, productDesc) {
+    let searchResultItem = document.createElement('article')
+        searchResultItem.innerHTML = 
+        `<img class="search-result-image" id="brand-image" src="${productLogo}" alt="${brandName}">
+        <img class="search-result-image" id="product-image" src="${productPic}" alt="${productName}">
+        <h3>${productName} - (${productColor})</h3>
+        <h4>$${productPrice}</h4>
+        <p>${productDesc}</p>
+        <h5>Details</h5>
+        <ul>
+            <li>(None)</li>
+        </ul>`
+
+        searchResultsContainer.appendChild(searchResultItem)
+}
