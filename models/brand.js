ca85d97802866f879fca5fb48e3832a4939e01a0
msayra <msayra333@gmail.com>
2024-10-16 10:56:02 -0500

    Added coma

--- models/brand.js
+++ models/brand.js
@@ -5,7 +5,7 @@ const brandSchema = new Schema(
     name: { type: String, required: true },
     headquarters: { type: String, required: true },
     owner: { type: String, required: true },
-    yearEstablished: { type: Number, required: true, min: 0 }
+    yearEstablished: { type: Number, required: true, min: 0 },
     logo: {type: String, required: true },
   },
   { timestamps: true }
